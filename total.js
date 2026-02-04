310422ebd7ab8931b144ff550cb315e639f554ad
mdallamaiqbal <bd999plus13@gmail.com>
2026-02-04 14:38:39 +0600

    add cart total

--- total.js
+++ total.js
@@ -14,3 +14,20 @@ function getShoppingTotal(products){
 }
 const total=getShoppingTotal(products);
 console.log('total ajke khosbe: ',total);
+
+const productsQuantity=[
+    {name:'Shampo',price:2400,quantity:5},
+    {name:'Oil',price:2000,quantity:2},
+    {name:'FaceWash',price:1400,quantity:2},
+    {name:'Powder',price:400,quantity:4}
+];
+function cartTotal(products){
+   let total =0;
+   for(const product of products){
+     const thisProductCost=product.price*product.quantity;
+    total=total+thisProductCost
+   }
+   return total;
+}
+const shoppingCart=cartTotal(productsQuantity);
+console.log(shoppingCart);
